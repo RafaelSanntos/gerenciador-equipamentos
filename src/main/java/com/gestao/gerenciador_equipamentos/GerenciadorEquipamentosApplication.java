a1b774c0cc954f7830c261362cb413e7b0249d37
Rafael Santos <job.rafael@gmail.com>
2025-10-29 20:15:17 -0300

    chore: inicializa projeto Spring Boot gerenciador-equipamentos

---
+++ src/main/java/com/gestao/gerenciador_equipamentos/GerenciadorEquipamentosApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.gerenciador_equipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEquipamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/gerenciador_equipamentos/GerenciadorEquipamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.gerenciador_equipamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEquipamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
